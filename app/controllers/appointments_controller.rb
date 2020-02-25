2134826874932f28b0019cb7707b5700c7e498c6
Brianna Bell <bbell9563@gmail.com>
2020-02-24 17:29:29 -0700

    basic CRUD is workign

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,54 @@
 class AppointmentsController < ApplicationController
+  before_action :set_patients, only: [:edit, :update, :new, :create]
+  before_action :set_doctors, only: [:edit, :update, :new, :create]
+
+  before_action :set_appointment, only: [:show]
+
+
   def show
+    @patient = Patient.find(@appointment.patient_id)
+    @doctor = Doctor.find(@appointment.doctor_id)
   end
 
   def new
+    @appointment = Appointment.new 
   end
 
+  def create
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save 
+      redirect_to appointments_path
+    else  
+      render :new
+    end
+  end
   def index
+    @appointments = Appointment.all 
   end
 
   def edit
   end
+
+  private
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end
+  def appointment_params
+    params.require(:appointment).permit(:patient_id, :doctor_id, :time, :date)
+  end
+  def set_patients
+    @patients = Patient.all
+  end
+
+  def set_doctors
+    @doctors = Doctor.all
+  end
+
+  # def set_doctors
+  #   @doctor = Doctor.find(params[:doctor_id])
+  # end
+
+  # def set_patients
+  #   @patient = Patient.find(params[:patient_id])
+  # end
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,12 @@
 class Doctor < ApplicationRecord
-  has_many :patients, through: :appointments
-  has_many :appointments 
+  has_many :patients, :through => :appointments
+  has_many :appointments, dependent: :destroy
+
+  def full_name 
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def title
+    "Dr. #{self.last_name}"
+  end
 end
